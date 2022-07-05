1530459fe36e3b57226273693942ceab5a1f157e
Rafael AGV <rafael.agv@protonmail.com>
2022-07-05 12:36:21 -0300

    create User insert

--- src/main/java/com/rafael/workshopmongo/service/UserService.java
+++ src/main/java/com/rafael/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafael.workshopmongo.domain.User;
+import com.rafael.workshopmongo.dto.UserDTO;
 import com.rafael.workshopmongo.repository.UserRepository;
 import com.rafael.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -25,6 +26,16 @@ public class UserService {
 	{
 		Optional <User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));	
+	}
+	
+	public User insert(User obj)
+	{
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto)
+	{
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
