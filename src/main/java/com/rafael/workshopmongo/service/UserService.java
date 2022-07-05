8e87966cd3f16e05ab93355fb8a2f92ccdbd0a75
Rafael AGV <rafael.agv@protonmail.com>
2022-07-05 11:04:41 -0300

    obtain User by Id

--- src/main/java/com/rafael/workshopmongo/service/UserService.java
+++ src/main/java/com/rafael/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.rafael.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafael.workshopmongo.domain.User;
 import com.rafael.workshopmongo.repository.UserRepository;
+import com.rafael.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,6 +19,12 @@ public class UserService {
 	public List<User> findAll()
 	{
 		return repo.findAll();
+	}
+	
+	public User findById(String id)
+	{
+		Optional <User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));	
 	}
 
 }
---
+++ src/main/java/com/rafael/workshopmongo/service/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.rafael.workshopmongo.service.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	
+	public ObjectNotFoundException(String msg)
+	{
+		super(msg);
+	}
+
+}
