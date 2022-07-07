93158a4d7270f9f1326bbab1bfbbd769f23b04f3
Rafael AGV <rafael.agv@protonmail.com>
2022-07-07 16:23:08 -0300

    Return User's posts

--- src/main/java/com/rafael/workshopmongo/resources/UserResource.java
+++ src/main/java/com/rafael/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.rafael.workshopmongo.domain.Post;
 import com.rafael.workshopmongo.domain.User;
 import com.rafael.workshopmongo.dto.UserDTO;
 import com.rafael.workshopmongo.service.UserService;
@@ -67,7 +68,14 @@ public class UserResource {
 		obj = service.update(obj);
 		
 		return ResponseEntity.noContent().build();
-		
 	}
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findByPosts(@PathVariable String id)
+	{
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
+	
 
 }
